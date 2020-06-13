43fb4968ad77b1d035e3b1368926ef580383ffee
Andrés Londoño <andres.londono@teaminternational.com>
2020-06-13 12:54:00 -0500

    uso de directivas

--- CedesistemasApp/src/app/home/home.page.ts
+++ CedesistemasApp/src/app/home/home.page.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"],
 })
 export class HomePage {
+  nombre: string;
+  apellidos: string;
+  edad: number;
 
-  constructor() {}
+  estudiantes: Estudiante[];
 
+  constructor() {
+    this.estudiantes = [];
+  }
+
+  crearEstudiante() {
+    this.estudiantes.push({
+      nombre: this.nombre,
+      apellidos: this.apellidos,
+      edad: this.edad,
+    });
+  }
+}
+
+//Modelo
+interface Estudiante {
+  nombre: string;
+  apellidos: string;
+  edad: number;
 }
