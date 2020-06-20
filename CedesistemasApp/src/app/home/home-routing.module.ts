c54794f773e425bdfd81164c844815aad0e17200
Andrés Londoño <andres.londono@teaminternational.com>
2020-06-19 23:52:58 -0500

    created tabs

--- CedesistemasApp/src/app/home/home-routing.module.ts
+++ CedesistemasApp/src/app/home/home-routing.module.ts
@@ -1,16 +1,33 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { HomePage } from './home.page';
+import { AboutComponent } from "./about/about.component";
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { HomePage } from "./home.page";
+import { RestaurantsComponent } from "./restaurants/restaurants.component";
 
 const routes: Routes = [
   {
-    path: '',
+    path: "tabs",
     component: HomePage,
-  }
+    children: [
+      {
+        path: "restaurants",
+        component: RestaurantsComponent,
+      },
+      {
+        path: "about",
+        component: AboutComponent,
+      },
+    ],
+  },
+  {
+    path: "",
+    redirectTo: "/home/tabs/restaurants",
+    pathMatch: "full",
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class HomePageRoutingModule {}
