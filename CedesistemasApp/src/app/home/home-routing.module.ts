c45e75ee1aee8c279c78aff00e545fa59226e69b
Andrés Londoño <andres.londono@teaminternational.com>
2020-06-26 22:24:22 -0500

    navigation

--- CedesistemasApp/src/app/home/home-routing.module.ts
+++ CedesistemasApp/src/app/home/home-routing.module.ts
@@ -8,13 +8,39 @@ import { RestaurantsComponent } from './restaurants/restaurants.component';
 const routes: Routes = [
   {
     path:'',
-    redirectTo:'/home/tabs/restaurants',
-    pathMatch: "full"
+    //redirectTo:'/home/tabs/restaurants',
+    //pathMatch: "full"
   },
   {
     path: 'tabs',
     component: HomePage,
     children:[
+
+      {
+        path: 'tab1',
+        children: [
+          {
+            path: '',
+            // loadChildren: './tab1/tab1.module#Tab1PageModule'
+            loadChildren: () => import('./tab1/tab1.module').then( m => m.Tab1PageModule) 
+          },
+          {
+            path: 'profile',
+            children: [
+              {
+                path: '',
+                loadChildren: () => import('./profile/profile.module').then( m => m.ProfilePageModule) 
+              },
+              {
+                path: 'profile-settings',
+                loadChildren: '../profile-settings/profile-settings.module#ProfileSettingsPageModule'
+              }
+            ]
+          }
+        ]
+      },
+
+
       {
         path:"restaurants",
         component: RestaurantsComponent
@@ -28,7 +54,12 @@ const routes: Routes = [
         component: AboutComponent
       }
     ]
+  },
+  {
+    path: 'profile',
+    loadChildren: () => import('./profile/profile.module').then( m => m.ProfilePageModule)
   }
+
 ];
 
 @NgModule({
